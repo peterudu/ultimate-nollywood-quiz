4c6807e07faaf6f0eab3166ed597eb2265202c05
peterudu <puduevborho7@gmail.com>
2022-12-17 11:02:48 +0000

    Find HTML elements in the DOM with id and class

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let containerWrapper = document.getElementsByClassName("container-wrapper");
+let quizContainer = document.getElementsByClassName("quiz-container");
+
+let quizQuestion = document.getElementById("question");
+let questionContent = quizQuestion.innerHTML;
+
+let answerButtons = document.getElementsByClassName("answer-buttons");
+
+let quizStatus = document.getElementsByClassName("quiz-status");
+
+let quizProgress = document.getElementById("quiz-progress");
+let progressContent = quizProgress.innerHTML;
+
+let nextButton = document.getElementById("next");
+let performanceStatement = document.getElementById("perf-statement");
+let overallPerformance = performanceStatement.innerHTML;
+
+
